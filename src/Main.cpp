0380e221cc86a893efc156ca6c9cb7f66395bd38
gonza <gonzalo.christobal@gmail.com>
2021-01-20 12:59:44 -0300

    Rename

--- src/Main.cpp
+++ src/Main.cpp
@@ -37,14 +37,6 @@ const char *vertexShaderSource = "attribute vec3 c;\n"
                                  "{\n"
                                  "   gl_Position = vec4(c, 1.0);\n"
                                  "}\0";
-#else
-const char *vertexShaderSource =
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
 #endif
 
 #ifdef __EMSCRIPTEN__
@@ -52,14 +44,6 @@ const char *fragmentShaderSource =
     "void main()\n"
     "{\n"
     "   gl_FragColor = vec4(1.0, 0.5, 0.2, 1.0);\n"
-    "}\n\0";
-#else
-const char *fragmentShaderSource =
-    "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
     "}\n\0";
 #endif
 
